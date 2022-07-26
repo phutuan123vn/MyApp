31d787c2aca924b0ca46420c8e6642865d40ca5f
Zasxac <monkeydroket@gmail.com>
2022-07-26 14:30:05 +0700

    SV nav, back

--- app/src/main/java/com/example/myapplication/sinhvien/Svdiemdanh.kt
+++ app/src/main/java/com/example/myapplication/sinhvien/Svdiemdanh.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.myapplication.sinhvien
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.example.myapplication.R
 import kotlinx.android.synthetic.main.svdiemdanh.view.*
 
 
