af14afbaee61df6bffd4e4d4695a2c71048d7d10
Zasxac <monkeydroket@gmail.com>
2022-07-31 22:22:43 +0700

    layout qldl sv gv

--- app/src/main/java/com/example/myapplication/quanly/GVqldl.kt
+++ app/src/main/java/com/example/myapplication/quanly/GVqldl.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.Navigation
 import com.example.myapplication.R
 import kotlinx.android.synthetic.main.gv_qldl.view.*
@@ -16,7 +18,15 @@ class GVqldl : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.gv_qldl, container, false)
+        val v = inflater.inflate(R.layout.gv_qldl, container, false)
+        val detail = v.findViewById<Button>(R.id.qldldetailgva)
+        detail.setOnClickListener {
+            val GVqldldetail = GVqldldetail()
+            val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+            transaction.replace(R.id.framelayoutqlnav, GVqldldetail)
+            transaction.commit()
+        }
+        return v
     }
 
 }
--- app/src/main/java/com/example/myapplication/quanly/GVqldldetail.kt
+++ app/src/main/java/com/example/myapplication/quanly/GVqldldetail.kt
@@ -9,21 +9,21 @@ import android.widget.Button
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.Navigation
 import com.example.myapplication.R
-import kotlinx.android.synthetic.main.sv_qldl.view.*
+import kotlinx.android.synthetic.main.gv_qldldetail.view.*
 
 
-class SVqldl : Fragment() {
+class GVqldldetail : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v = inflater.inflate(R.layout.sv_qldl, container, false)
-        val detail = v.findViewById<Button>(R.id.qldldetailgiang)
-        detail.setOnClickListener {
-            val SVqldldetail = SVqldldetail()
+        val v = inflater.inflate(R.layout.gv_qldldetail, container, false)
+        val back = v.findViewById<Button>(R.id.Gvqldlback)
+        back.setOnClickListener {
+            val GVqldl = GVqldl()
             val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
-            transaction.replace(R.id.framelayoutqlnav, SVqldldetail)
+            transaction.replace(R.id.framelayoutqlnav, GVqldl)
             transaction.commit()
         }
         return v
