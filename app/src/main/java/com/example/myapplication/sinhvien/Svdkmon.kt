97399d0da497ec6342ec462e8e3fb6e9baa823b3
Zasxac <monkeydroket@gmail.com>
2022-08-09 17:10:47 +0700

    sv table

--- app/src/main/java/com/example/myapplication/sinhvien/Svdkmon.kt
+++ app/src/main/java/com/example/myapplication/sinhvien/Svdkmon.kt
@@ -5,17 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.DatabaseHandler
 import com.example.myapplication.R
+import com.example.myapplication.model.User
+import com.example.myapplication.quanly.quanlysv.SVqltk
+import com.example.myapplication.quanly.quanlysv.TableRowAdapterSVdsmondk
+import com.example.myapplication.quanly.quanlysv.TableRowAdapterSVmondadk
+import com.example.myapplication.quanly.quanlysv.TableRowAdapterSVqltk
 import kotlinx.android.synthetic.main.svdkmon.view.*
 
 
 class Svdkmon : Fragment() {
+    private lateinit var tableRecyclerView : RecyclerView
+    private lateinit var tableRowAdapterSVdsmondk: TableRowAdapterSVdsmondk
+    private lateinit var tableRowAdapterSVmondadk: TableRowAdapterSVmondadk
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.svdkmon, container, false)
-    }
+        val v = inflater.inflate(R.layout.svdkmon, container, false)
+        val data=activity.createContext(this)
+        dataInitlize(data)
+        val layoutManager = LinearLayoutManager(context)
+        tableRecyclerView = v.findViewById(R.id.table_recycler_view_dsmondk)
+        tableRecyclerView.layoutManager = layoutManager
+        tableRecyclerView.setHasFixedSize(true)
+        tableRowAdapterSVdsmondk = TableRowAdapterSVdsmondk(data)
+        tableRecyclerView.adapter = tableRowAdapterSVdsmondk
 
+        val data1=activity.createContext(this)
+        dataInitlize(data1)
+        val layoutManager2 = LinearLayoutManager(context)
+        tableRecyclerView = v.findViewById(R.id.table_recycler_view_mondadk)
+        tableRecyclerView.layoutManager = layoutManager2
+        tableRecyclerView.setHasFixedSize(true)
+        tableRowAdapterSVmondadk = TableRowAdapterSVmondadk(data1)
+        tableRecyclerView.adapter = tableRowAdapterSVmondadk
+
+        return v
+    }
+    private fun dataInitlize(array: ArrayList<User>) {
+    }
+}
+
+
+private fun FragmentActivity?.createContext(sVdkmon: Svdkmon): ArrayList<User> {
+    val db= DatabaseHandler(this!!)
+    return db.getAllData()
 }
