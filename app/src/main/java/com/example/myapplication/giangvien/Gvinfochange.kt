a4a9a68977c824dded9aebe0d0082f33adc3d007
Phu Tuan <phutuan123vn>
2022-08-18 11:30:55 +0700

    change

--- app/src/main/java/com/example/myapplication/giangvien/Gvinfochange.kt
+++ app/src/main/java/com/example/myapplication/giangvien/Gvinfochange.kt
@@ -9,9 +9,15 @@ import android.widget.Button
 import androidx.fragment.app.FragmentTransaction
 import com.example.myapplication.R
 import androidx.navigation.Navigation
-import com.example.myapplication.sinhvien.Svinfo
-import com.example.myapplication.sinhvien.Svinfochange
+import com.example.myapplication.MYSQLHandler
+import com.example.myapplication.model.TEMP
+import kotlinx.android.synthetic.main.gvinfo.*
+import kotlinx.android.synthetic.main.gvinfochange.*
 import kotlinx.android.synthetic.main.gvinfochange.view.*
+import kotlinx.android.synthetic.main.svinfochange.*
+import kotlinx.android.synthetic.main.svinfochange.hoedit
+import kotlinx.android.synthetic.main.svinfochange.noiedit
+import kotlinx.android.synthetic.main.svinfochange.tenedit
 
 
 class Gvinfochange : Fragment() {
@@ -23,6 +29,24 @@ class Gvinfochange : Fragment() {
     ): View? {
         val v = inflater.inflate(R.layout.gvinfochange, container, false)
         val back = v.findViewById<Button>(R.id.gvinfochangeback)
+        val db= MYSQLHandler(requireContext())
+        db.getTeachInfo(MYSQLHandler.user.id,object : MYSQLHandler.VolleyCallback1{
+            override fun onSuccess(Data: ArrayList<TEMP>) {
+                super.onSuccess(Data)
+                var lname=Data.get(0).t1
+                var fname=Data.get(0).t2
+                var addr=Data.get(0).t5
+                var pid=Data.get(0).t6
+                var phone=Data.get(0).t7
+                var emaill=Data.get(0).t8
+                GVhoedit.setText(lname)
+                GVtenedit.setText(fname)
+                GVnoiedit.setText(addr)
+                GVcmndedit.setText(pid)
+                GVsdtedit.setText(phone)
+                GVmailedit.setText(emaill)
+            }
+        })
         back.setOnClickListener {
             val Gvinfo = Gvinfo()
             val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
